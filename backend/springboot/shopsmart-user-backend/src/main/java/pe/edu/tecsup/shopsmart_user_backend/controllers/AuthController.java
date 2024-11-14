94d6f414dc401bc0a4f512847d91f53e1c12f0df
Alonso-S <alonso.s2k5@gmail.com>
2024-11-14 12:29:40 -0500

    feat(controller): Add AuthController for login and registration endpoints

---
+++ backend/springboot/shopsmart-user-backend/src/main/java/pe/edu/tecsup/shopsmart_user_backend/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package pe.edu.tecsup.shopsmart_user_backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.tecsup.shopsmart_user_backend.dto.LoginRequest;
+import pe.edu.tecsup.shopsmart_user_backend.dto.RegisterRequest;
+import pe.edu.tecsup.shopsmart_user_backend.dto.TokenResponse;
+import pe.edu.tecsup.shopsmart_user_backend.services.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<TokenResponse> register(@RequestBody final RegisterRequest request){
+        final TokenResponse tokenResponse = authService.register(request);
+        return ResponseEntity.ok(tokenResponse);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody final LoginRequest request){
+        final TokenResponse tokenResponse = authService.login(request);
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+
+}
