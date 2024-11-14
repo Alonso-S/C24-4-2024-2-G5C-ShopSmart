3dca13dc1e204428f606ae3fc129a647b14da676
Alonso-S <alonso.s2k5@gmail.com>
2024-11-14 12:15:23 -0500

    feat(services): Add JwtService for token generation and validation

---
+++ backend/springboot/shopsmart-user-backend/src/main/java/pe/edu/tecsup/shopsmart_user_backend/services/JwtService.java
@@ -0,0 +1,79 @@
+package pe.edu.tecsup.shopsmart_user_backend.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import pe.edu.tecsup.shopsmart_user_backend.models.User;
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private Long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
+    public String generateToken(final User user) {
+        return buildToken(user, jwtExpiration);
+    }
+
+    public String generateRefreshToken(final User user) {
+        return buildToken(user, refreshExpiration);
+    }
+
+    private String buildToken(final User user, final long expiration) {
+        return Jwts.builder()
+                .id(user.getId().toString())
+                .claims(Map.of(
+                        "firstName", user.getFirstName(),
+                        "lastName", user.getLastName()
+                ))
+                .subject(user.getEmail())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey())
+                .compact();
+
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public boolean isTokenValid(String token, User user) {
+        final String username = extractUsername(token);
+        return (username.equals(user.getEmail())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
